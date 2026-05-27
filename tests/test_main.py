0783aad133c4763e8b54f936eaf4c6779432bd26
Ryan Fleck <Ryan.Fleck@protonmail.com>
2026-05-27 08:41:40 -0600

    Add some more cases

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,6 +7,27 @@ def client():
     return app.test_client()
 
 
-def test_request_example(client):
+def test_homepage_request(client):
     response = client.get("/")
+    assert response.status_code == 200
     assert b'"status":"OK"' in response.data
+
+
+def test_secret_request(client):
+    response = client.get("/download/shared-config?key=secret-password")
+    assert response.status_code == 200
+
+
+def test_secret_bad_path(client):
+    response = client.get("/download/shared-con?key=secret-password")
+    assert response.status_code == 404
+
+
+def test_secret_bad_key(client):
+    response = client.get("/download/shared-config?key=secret-password2")
+    assert response.status_code == 404
+
+
+# DOWNLOAD_PATH = "shared-config"
+# DOWNLOAD_KEY = "secret-password"
+# PROTECTED_FILE = "tests/data/info.txt"
